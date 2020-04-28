b86facc4936a96f0f92c90a8c47fbab9bd375a26
avahab <avahab@deloitte.com>
2020-04-28 20:55:09 +0530

    restapi provider integrated and backend added to maintain state files.

--- terraform-webfront-azure-HCL/main.tf
+++ terraform-webfront-azure-HCL/main.tf
@@ -38,9 +38,9 @@ output "puppet_response" {
   value = module.azure-load-balancer-module.puppet_response
 }
 
-# terraform {
-#   backend "azurerm" {}
-# }
+terraform {
+  backend "azurerm" {}
+}
 
 
 
