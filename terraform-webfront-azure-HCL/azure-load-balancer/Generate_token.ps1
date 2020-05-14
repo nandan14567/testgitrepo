155f988fc68c813eaed2bc27380e8ef4b0c74fee
avahab <avahab@deloitte.com>
2020-05-14 18:39:22 +0530

    initial commit-powershell changes

--- terraform-webfront-azure-HCL/azure-load-balancer/Generate_token.ps1
+++ terraform-webfront-azure-HCL/azure-load-balancer/Generate_token.ps1
@@ -1,44 +1,26 @@
-Param(
-    [Parameter(Mandatory = $True,
-        HelpMessage = "Please supply a JSON parameter file.",
-        ValueFromPipeline = $True,
-        ValueFromPipelineByPropertyName = $True)]
-    [string]$resource,
-       
-        
-    [Parameter(Mandatory = $True,
-        HelpMessage = "Please supply a client id",
-        ValueFromPipeline = $True,
-        ValueFromPipelineByPropertyName = $True)]
-    [string]$clientid,
-   
+$jsonpayload = [Console]::In.ReadLine()
+$json = ConvertFrom-Json $jsonpayload
 
-    [Parameter(Mandatory = $True,
-        HelpMessage = "Please supply a client secret",
-        ValueFromPipeline = $True,
-        ValueFromPipelineByPropertyName = $True)]
-    [string]$clientsecret
-)
+# Access JSON values 
+$resource = $json.resource
+$clientid = $json.clientid
+$clientsecret = $json.clientsecret
 
 $jsonobj = @{
-    grant_type    = "client_credentials"
-    resource      = "$resource"
-    client_id     = "$clientid"
-    client_secret = "$clientsecret"
+        grant_type    = "client_credentials"
+        resource      = "$resource"
+        client_id     = "$clientid"
+        client_secret = "$clientsecret"
 }
-
 $contentType = 'application/x-www-form-urlencoded' 
-
 $uri = 'https://login.microsoftonline.com/36da45f1-dd2c-4d1f-af13-5abe46b99921/oauth2/token'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$tokenReq = Invoke-WebRequest -Uri $uri -Method Post -Body $jsonobj -ContentType $contentType -UseBasicParsing
+$tokenReq = Invoke-WebRequest -Uri $uri -Method Post -Body $jsonobj -ContentType $contentType
 $Gettoken = $tokenreq.content | Convertfrom-json
 $token = $Gettoken.access_token
-$text = "Authorization:" + "$token"
-$text | out-file -encoding ASCII output_token_sn.txt
-
+$dict = @{}
+$dict.Add(0,$token)
+$object = [PSCustomObject]$dict
+$test=$object | ConvertTo-Json
+Write-Output $test
     
-
-    
-
-
