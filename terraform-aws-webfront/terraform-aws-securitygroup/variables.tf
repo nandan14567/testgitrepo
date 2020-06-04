5e8b7985ab7937f9764cb5530f1d93d123b6fca5
Abdul Vahab <avahab@deloitte.com>
2020-06-04 13:15:55 +0000

    Updated variables.tf

--- terraform-aws-webfront/terraform-aws-securitygroup/variables.tf
+++ terraform-aws-webfront/terraform-aws-securitygroup/variables.tf
@@ -1,13 +1,15 @@
 variable "tcp_ports" {
-
+type    = list(string)
 }
 
 variable "cidrs" {
-
+type    = list(string)
 }
 
-variable "security_group_name" {}
+variable "security_group_name" {
+type = string
+}
 
 variable "vpc_id" {
-
+type = string
 }
