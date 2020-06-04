8fb492f8ea6d2c0e72deb9c9cbe56bb5b0e50e55
Abdul Vahab <avahab@deloitte.com>
2020-06-04 13:21:06 +0000

    Updated variables.tf

--- terraform-aws-webfront/variables.tf
+++ terraform-aws-webfront/variables.tf
@@ -1,30 +1,64 @@
-variable "region"{}
-variable "recipe_name"{}
+variable "region"{
+    type = string
+}
+variable "recipe_name"{
+    type = string
+}
 variable "recipe_cidrs"{}
 variable "instance_count"{}
 variable "instance_ports"{}
-variable "vpc_id"{}
-variable "instance_subnet_id"{}
-variable "ami_id" {}
-variable "instance_type"{}
-variable "instance_role"{}
-variable "key_name"{}
-variable "certificate_arn"{}
-variable "ssl_policy"{}
+variable "vpc_id"{
+    type = string
+}
+variable "instance_subnet_id"{
+    type = string
+}
+variable "ami_id" {
+    type = string
+}
+variable "instance_type"{
+    type = string
+}
+variable "instance_role"{
+    type = string
+}
+variable "key_name"{
+    type = string
+}
+variable "certificate_arn"{
+    type = string
+}
+variable "ssl_policy"{
+    type = string
+}
 
 //Variables for Puppet Payload
-variable "accountid_puppet"{}
-variable "domain_puppet"{}
-variable "environment_puppet"{}
-variable "operatingsystem"{}
+variable "accountid_puppet"{
+    type = string
+}
+variable "domain_puppet"{
+    type = string
+}
+variable "environment_puppet"{
+    type = string
+}
+variable "operatingsystem"{
+    type = string
+}
 
 //Variables for Servernaming Payload
 variable "servernaming" {}
 
 //Variables for Token Generation
-variable "client_id"{}
-variable "client_secret"{}
-variable "resource_token"{}
+variable "client_id"{
+    type = string
+}
+variable "client_secret"{
+    type = string
+}
+variable "resource_token"{
+    type = string
+}
 
 //Variable for Security Group Yaml File
 variable "securitygroup_administrators" {}
